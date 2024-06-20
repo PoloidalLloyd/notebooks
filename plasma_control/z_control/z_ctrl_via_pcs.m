ab613543402cd4500c5cc214ec0e189f27c1682a
PoloidalLloyd <jlb647@york.ac.uk>
2024-06-20 11:15:27 +0100

    adding scripts again

---
+++ plasma_control/z_control/z_ctrl_via_pcs.m
@@ -0,0 +1,62 @@
+clear;
+close all;
+
+% load the data
+load("siso_model.mat");
+
+ss_model = mastu_VS_siso_sys;
+
+% control design 
+%-----------------------------------------------------------------
+% Convert the state space model to a transfer function
+[num, den] = ss2tf(ss_model.A, ss_model.B, ss_model.C, ss_model.D);
+
+siso_tf = tf(num, den);
+
+
+
+% Due to phase cancellation at 0 between poles and zeroes we can use the minreal() function 
+% to remove these from the transfer function
+
+min_tol = 1e-6;
+min_siso_tf = minreal(siso_tf, min_tol);
+
+[min_num, min_den] = tfdata(min_siso_tf, 'v');
+
+% true if want manual input of control parameters
+if 1
+
+    % Define a PID controller and weights
+    kp = 1500.5;
+    ki = 600.5762;
+    kd = 34.1550;
+
+    % Derivative timescale
+    td = 0.005;  % 5 milliseconds 
+    
+    % Integral timescale
+    ti = 0.5;  % 500 milliseconds
+    
+    % Derivative and integral gains based on timescales (uncomment for
+    % parameterisation
+
+    % kd = kp * td;  
+    % ki = kp / ti;  
+    
+    controller = pid(kp, ki, kd);
+
+end
+
+
+% Define the open loop response (for nyquist diagram)
+mastu_open_loop = controller*min_siso_tf;
+
+% Now add this to the system transfer function as feedback
+mastu_closed_loop = feedback(controller*min_siso_tf, 1);
+
+
+pzmap(mastu_closed_loop)
+
+step(mastu_closed_loop)
+
+
